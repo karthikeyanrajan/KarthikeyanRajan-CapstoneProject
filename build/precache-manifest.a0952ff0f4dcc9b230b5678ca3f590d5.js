a06b477b87d30ba0509882048cb0c69d80aba70e
karthikeyanrajan <karthisk218@gmail.co>
2023-10-25 15:52:25 +0000

    files

---
+++ build/precache-manifest.a0952ff0f4dcc9b230b5678ca3f590d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1257c7f61a021b25e516bb72b755bd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ff3208074254b65c4ff",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bfc1800433480d5d9171",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5ff3208074254b65c4ff",
+    "url": "/static/js/2.c71cdbdb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.c71cdbdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfc1800433480d5d9171",
+    "url": "/static/js/main.3db866c1.chunk.js"
+  },
+  {
+    "revision": "205dba4f9959a084aa7c",
+    "url": "/static/js/runtime-main.c2a4c5c8.js"
+  }
+]);
